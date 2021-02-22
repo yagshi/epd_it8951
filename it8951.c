f550f48164f09e046741428d8309b473e0ef7986
Hiroyuki Kobayashi <yagshi@epid>
2021-02-22 07:30:57 +0000

    cmake version works

--- it8951.c
+++ it8951.c
@@ -1,9 +1,9 @@
+#include "it8951_p.h"
 #include "it8951.h"
 #include <stdio.h>
 
 //Global varivale
 IT8951DevInfo gstI80DevInfo;
-uint8_t* gpFrameBuf; //Host Source Frame buffer
 uint32_t gulImgBufAddr; //IT8951 Image buffer address
 
 
@@ -138,27 +138,46 @@ void lcd_wWrite_cmd_code(uint16_t usCmdCode) {
   bcm2835_gpio_write(CS, HIGH); 
 }
 
+//-----------------------------------------------------------
+//Host controller function 5---Write command to host data Bus with aruments
+//-----------------------------------------------------------
+void lcd_send_cmd_arg(uint16_t usCmdCode,
+                      uint16_t* pArg,
+                      uint16_t usNumArg) {
+  uint16_t i;
+  //Send Cmd code
+  lcd_write_cmd_code(usCmdCode);
+  //Send Data
+  for(i = 0; i < usNumArg; i++) {
+    lcd_write_data(pArg[i]);
+  }
+}
 
 
-void it8951_get_system_info(void* pBuf) {
-  uint16_t* pusWord = (uint16_t*)pBuf;
-  IT8951DevInfo* pstDevInfo;
-  
-  //Send I80 CMD
-  lcd_write_cmd_code(USDEF_I80_CMD_GET_DEV_INFO);
-  
-  //Burst Read Request for SPI interface only
-  lcd_read_n_data(pusWord, sizeof(IT8951DevInfo) / 2); //Polling HRDY for each words(2-bytes) if possible
-  
-  //Show Device information of IT8951
-  pstDevInfo = (IT8951DevInfo*)pBuf;
-  printf("Panel(W,H) = (%d, %d)\r\n",
-         pstDevInfo->usPanelW, pstDevInfo->usPanelH );
-  printf("Image Buffer Address = %X\r\n",
-         pstDevInfo->usImgBufAddrL | (pstDevInfo->usImgBufAddrH << 16));
-  //Show Firmware and LUT Version
-  printf("FW Version = %s\r\n", (uint8_t*)pstDevInfo->usFWVersion);
-  printf("LUT Version = %s\r\n", (uint8_t*)pstDevInfo->usLUTVersion);
+//-----------------------------------------------------------
+//Initial function 2---Set Image buffer base address
+//-----------------------------------------------------------
+void it8951_set_img_buf_base_addr(uint32_t ulImgBufAddr) {
+  uint16_t usWordH = (uint16_t)((ulImgBufAddr >> 16) & 0x0000FFFF);
+  uint16_t usWordL = (uint16_t)( ulImgBufAddr & 0x0000FFFF);
+  //Write LISAR Reg
+  it8951_write_reg(LISAR + 2, usWordH);
+  it8951_write_reg(LISAR, usWordL);
+}
+
+
+
+//-----------------------------------------------------------
+//Host Cmd 4---REG_RD
+//-----------------------------------------------------------
+uint16_t it8951_read_reg(uint16_t usRegAddr) {
+  uint16_t usData;
+  //Send Cmd and Register Address
+  lcd_write_cmd_code(IT8951_TCON_REG_RD);
+  lcd_write_data(usRegAddr);
+  //Read data from Host Data bus
+  usData = lcd_read_data();
+  return usData;
 }
 
 //-----------------------------------------------------------
@@ -170,6 +189,14 @@ void it8951_write_reg(uint16_t usRegAddr, uint16_t usValue) {
   lcd_write_data(usRegAddr);
   lcd_write_data(usValue);
 }
+
+//-----------------------------------------------------------
+//Host Cmd 12---LD_IMG_END
+//-----------------------------------------------------------
+void it8951_load_img_end(void) {
+  lcd_write_cmd_code(IT8951_TCON_LD_IMG_END);
+}
+
 
 uint16_t it8951_get_vcom(void) {
   uint16_t vcom;
@@ -188,7 +215,7 @@ void it8951_set_vcom(uint16_t vcom) {
 }
 
 
-uint8_t it8951_init() {
+int it8951_init() {
   if (!bcm2835_init()) {
     perror("bcm2835_init() failed.");
     return -1;
@@ -202,26 +229,120 @@ uint8_t it8951_init() {
   bcm2835_gpio_fsel(RESET, BCM2835_GPIO_FSEL_OUTP);
 	
   bcm2835_gpio_write(CS, HIGH);
-  printf("****** IT8951 ******\n");
+  //printf("****** IT8951 ******\n");
   bcm2835_gpio_write(RESET, LOW);
   bcm2835_delay(100);
   bcm2835_gpio_write(RESET, HIGH);
   //Get Device Info
-  it8951_get_system_info(&gstI80DevInfo);
-  gpFrameBuf = malloc(gstI80DevInfo.usPanelW * gstI80DevInfo.usPanelH);
-  if (!gpFrameBuf) {
-    perror("malloc error!\n");
-    return 1;
-  }
-  gulImgBufAddr = gstI80DevInfo.usImgBufAddrL | (gstI80DevInfo.usImgBufAddrH << 16);
+  gstI80DevInfo = it8951_get_device_info();
+  gulImgBufAddr = gstI80DevInfo.imgBufAddrL | (gstI80DevInfo.imgBufAddrH << 16);
  	
   //Set to Enable I80 Packed mode
   it8951_write_reg(I80CPCR, 0x0001);
   if (VCOM != it8951_get_vcom()) {
     it8951_set_vcom(VCOM);
-    printf("VCOM = -%.02fV\n", (float)it8951_get_vcom() / 1000);
+    //printf("VCOM = -%.02fV\n", (float)it8951_get_vcom() / 1000);
   }
   return 0;
 }
 
+//-----------------------------------------------------------
+//Host Cmd 11---LD_IMG_AREA
+//-----------------------------------------------------------
+void it8951_load_img_area_start(IT8951LdImgInfo* pstLdImgInfo,
+                                IT8951AreaImgInfo* pstAreaImgInfo) {
+  uint16_t usArg[5];
+  //Setting Argument for Load image start
+  usArg[0] = (pstLdImgInfo->usEndianType << 8 )
+    |(pstLdImgInfo->usPixelFormat << 4)
+    |(pstLdImgInfo->usRotate);
+  usArg[1] = pstAreaImgInfo->usX;
+  usArg[2] = pstAreaImgInfo->usY;
+  usArg[3] = pstAreaImgInfo->usWidth;
+  usArg[4] = pstAreaImgInfo->usHeight;
+  //Send Cmd and Args
+  lcd_send_cmd_arg(IT8951_TCON_LD_IMG_AREA, usArg, 5);
+}
 
+//-----------------------------------------------------------
+//Display function 1---Wait for LUT Engine Finish
+//                     Polling Display Engine Ready by LUTNo
+//-----------------------------------------------------------
+int it8951_is_busy() {
+  return it8951_read_reg(LUTAFSR);
+}
+
+
+IT8951DevInfo it8951_get_device_info() {
+  IT8951DevInfo dev_info;
+  
+  //Send I80 CMD
+  lcd_write_cmd_code(USDEF_I80_CMD_GET_DEV_INFO);
+  
+  //Burst Read Request for SPI interface only
+  lcd_read_n_data((uint16_t*)&dev_info, sizeof(IT8951DevInfo) / 2);
+  
+  //Show Device information of IT8951
+  //printf("Panel(W,H) = (%d, %d)\r\n",
+  //dev_info.panelW, dev_info.panelH );
+  //printf("Image Buffer Address = %X\r\n",
+  //dev_info.imgBufAddrL | (dev_info.imgBufAddrH << 16));
+  //Show Firmware and LUT Version
+  //printf("FW Version = %s\r\n", (char*)dev_info.fwVersion);
+  //printf("LUT Version = %s\r\n", (char*)dev_info.lutVersion);
+  return dev_info;
+}
+
+//-----------------------------------------------------------
+//Display function 2---Load Image Area process
+//-----------------------------------------------------------
+void it8951_host_area_packed_pixel_write(IT8951LdImgInfo* pstLdImgInfo,
+                                         IT8951AreaImgInfo* pstAreaImgInfo) {
+  uint32_t i,j;
+  //Source buffer address of Host
+  uint16_t* pusFrameBuf = (uint16_t*)pstLdImgInfo->ulStartFBAddr;
+
+  //Set Image buffer(IT8951) Base address
+  it8951_set_img_buf_base_addr(pstLdImgInfo->ulImgBufBaseAddr);
+  //Send Load Image start Cmd
+  it8951_load_img_area_start(pstLdImgInfo , pstAreaImgInfo);
+  //Host Write Data
+  for(j = 0; j < pstAreaImgInfo->usHeight; j++) {
+    for(i = 0; i < pstAreaImgInfo->usWidth / 2; i++) {
+      //Write a Word(2-Bytes) for each time
+      lcd_write_data(*pusFrameBuf);
+      pusFrameBuf++;
+    }
+  }
+  it8951_load_img_end();
+}
+
+//-----------------------------------------------------------
+//Display functions 3---Application for Display panel Area
+//-----------------------------------------------------------
+void it8951_display_area(int x, int y, int w, int h, int mode) {
+  //Send I80 Display Command (User defined command of IT8951)
+  lcd_write_cmd_code(USDEF_I80_CMD_DPY_AREA); //0x0034
+  //Write arguments
+  lcd_write_data(x);
+  lcd_write_data(y);
+  lcd_write_data(w);
+  lcd_write_data(h);
+  lcd_write_data(mode);
+}
+
+
+void it8951_transfer_image(int x, int y, int w, int h, uint8_t *buf) {
+  IT8951LdImgInfo img_info;
+  IT8951AreaImgInfo area_info;
+  img_info.ulStartFBAddr = (uint32_t)buf;
+  img_info.usEndianType = IT8951_LDIMG_L_ENDIAN;
+  img_info.usPixelFormat    = IT8951_8BPP;
+  img_info.usRotate         = IT8951_ROTATE_0;
+  img_info.ulImgBufBaseAddr = gulImgBufAddr;
+  area_info.usX = x;
+  area_info.usY = y;
+  area_info.usWidth = w;
+  area_info.usHeight = h;
+  it8951_host_area_packed_pixel_write(&img_info, &area_info);
+}
--- test.c
+++ test.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "it8951.h"
+
 int main() {
+  uint8_t *buf;
+  IT8951DevInfo dev;
+  
   it8951_init();
+  dev = it8951_get_device_info();
+  if(!(buf = malloc(dev.panelW * dev.panelH))) {
+    perror("malloc failed.");
+    return -1;
+  }
+  for (int i = 0; i < dev.panelW * dev.panelH; i++) {
+    buf[i] = rand();
+  }
+  while(it8951_is_busy());
+  it8951_transfer_image(0, 0, dev.panelW, dev.panelH, buf);
+  while(it8951_is_busy());
+  it8951_display_area(0, 0, dev.panelW, dev.panelH, 2);
   return 0;
 }
